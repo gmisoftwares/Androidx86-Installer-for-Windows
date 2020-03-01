8878f81763b68b4426c74403f78292b98224bd61
Jon West <electrikjesus@gmail.com>
2020-02-29 21:14:44 -0500

    Update to v2.8

--- source/Android_UEFIInstaller/Properties/AssemblyInfo.cs
+++ source/Android_UEFIInstaller/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Bliss Studios")]
 [assembly: AssemblyProduct("Android_UEFIInstaller")]
-[assembly: AssemblyCopyright("Copyright © Bliss Studios 2017")]
+[assembly: AssemblyCopyright("Copyright © Bliss Studios 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.7.0000.0")]
-[assembly: AssemblyFileVersion("2.7.0.0")]
+[assembly: AssemblyVersion("2.8.5200.0")]
+[assembly: AssemblyFileVersion("2.8.0.0")]
