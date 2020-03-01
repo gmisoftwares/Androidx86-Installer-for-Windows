92f9ccfd7381fcf5b5f4c60f50c724cdfe6720e4
Jon West <electrikjesus@gmail.com>
2020-02-29 20:01:58 -0500

    Moar updates for Android 10

--- source/Android_UEFIInstaller/Properties/AssemblyInfo.cs
+++ source/Android_UEFIInstaller/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Android_UEFIInstaller")]
-[assembly: AssemblyDescription("Android x86 Installer for Windows [UEFI Version]")]
+[assembly: AssemblyDescription("Bliss OS Installer for Windows [UEFI Version]")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Bliss Studios")]
 [assembly: AssemblyProduct("Android_UEFIInstaller")]
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.6.0000.0")]
-[assembly: AssemblyFileVersion("2.6.0.0")]
+[assembly: AssemblyVersion("2.7.0000.0")]
+[assembly: AssemblyFileVersion("2.7.0.0")]
